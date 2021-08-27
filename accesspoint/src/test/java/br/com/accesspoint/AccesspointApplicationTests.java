0c67c654a09f596aef18dd9b164ffb8ba9a95e3a
Lucas Henrique de Oliveira Matos <lucasmatos@pactosolucoes.com.br>
2021-08-26 21:16:13 -0300

    Initial commit

---
+++ accesspoint/src/test/java/br/com/accesspoint/AccesspointApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.accesspoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesspointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
