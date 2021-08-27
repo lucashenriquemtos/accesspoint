63c7f5fff5dd78e072d15d2946b0b8ebfacb9801
Lucas Henrique de Oliveira Matos <lucasmatos@pactosolucoes.com.br>
2021-08-26 21:33:10 -0300

    Subindo Classes

---
+++ accesspoint/src/main/java/br/com/accesspoint/model/Endereco.java
@@ -0,0 +1,23 @@
+package br.com.accesspoint.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Endereco {
+    private String rua;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String telefone;
+
+}
---
+++ accesspoint/src/main/java/br/com/accesspoint/model/JornadaTrabalho.java
@@ -0,0 +1,19 @@
+package br.com.accesspoint.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class JornadaTrabalho {
+    private long id;
+    private String descricao;
+}
