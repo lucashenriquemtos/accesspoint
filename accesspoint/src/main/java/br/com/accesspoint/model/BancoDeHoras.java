d2033c317a78a35c80e6d49cc4168f1491ba543c
Lucas Henrique de Oliveira Matos <lucasmatos@pactosolucoes.com.br>
2021-08-26 22:16:01 -0300

    Criando classse BancoDeHoras

---
+++ accesspoint/src/main/java/br/com/accesspoint/model/BancoDeHoras.java
@@ -0,0 +1,49 @@
+package br.com.accesspoint.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
+@Builder
+@Entity
+public class BancoDeHoras {
+
+    @Embeddable
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    public class BancoHorasID implements Serializable {
+        private long idBancoHoras;
+        private long idMovimentacao;
+        private long idUsuario;
+    }
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @EmbeddedId
+    private BancoHorasID id;
+    private LocalDateTime dataTrabalhada;
+    private BigDecimal quantidadeHoras;
+    private BigDecimal saldoHoras;
+
+
+
+}
