ac3aa31606085e2ba266f40c7d9d87d6021f0c2d
Lucas Henrique de Oliveira Matos <lucasmatos@pactosolucoes.com.br>
2021-08-26 21:53:13 -0300

    Subindo classes

---
+++ accesspoint/src/main/java/br/com/accesspoint/model/Calendario.java
@@ -0,0 +1,25 @@
+package br.com.accesspoint.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Calendario {
+    private Long id;
+    private TipoData tipoData;
+    private String descricao;
+    private LocalDateTime dataEspecial;
+}
---
+++ accesspoint/src/main/java/br/com/accesspoint/model/Movimentacao.java
@@ -0,0 +1,42 @@
+package br.com.accesspoint.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Movimentacao {
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public class MovimentacaoId implements Serializable{
+        private Long idMovimento;
+        private Long idUsuario;
+
+    }
+
+    @EmbeddedId
+    private MovimentacaoId id;
+
+    private LocalDateTime dataEntrada;
+    private LocalDateTime dataSaida;
+    private BigDecimal periodo;
+    private Ocorrencia ocorrencia;
+    private Calendario calendario;
+}
